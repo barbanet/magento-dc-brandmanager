ff8a0338172534679943c79305fdf811ebe2c683
Damián Culotta <damianculotta@gmail.com>
2015-11-04 21:47:17 -0300

    Added compatibility with Magento 1.9.2.2 blocks policies.

---
+++ app/code/community/Dc/BrandManager/data/brandmanager_setup/data-upgrade-1.0.0-1.0.1.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Dc_BrandManager
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Dc
+ * @package    Dc_BrandManager
+ * @copyright  Copyright (c) 2012-2015 Damián Culotta. (http://www.damianculotta.com.ar/)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+$model = Mage::getModel('admin/block');
+if ($model) {
+    $data = array(
+        'block_name' => 'brandmanager/brands',
+        'is_allowed' => 1
+    );
+    $model->setData($data)->save();
+}
+
